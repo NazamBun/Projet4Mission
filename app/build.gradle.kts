d65a0390cca91e2e31320a58b9e23727c658c5d9
Bun Nazam <bun.nazam@gmail.com>
2024-08-19 17:43:09 +0200

    Initial commit - Project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("kotlin-kapt")
+  id("com.google.dagger.hilt.android")
 }
 
 android {
   namespace = "com.aura"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.aura"
     minSdk = 24
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 
@@ -24,11 +26,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_18
+    targetCompatibility = JavaVersion.VERSION_18
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "18"
   }
   buildFeatures {
     viewBinding = true
@@ -36,13 +38,30 @@ android {
 }
 
 dependencies {
+// Hilt
+  implementation("com.google.dagger:hilt-android:2.51.1")
+  kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
-  implementation("androidx.core:core-ktx:1.9.0")
-  implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.8.0")
-  implementation("androidx.annotation:annotation:1.6.0")
+// Retrofit + Moshi
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+  implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+  implementation("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
+
+// Existing dependencies
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+  implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
+  implementation("androidx.activity:activity-ktx:1.9.1")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
+  implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+  implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+  implementation("androidx.core:core-ktx:1.13.1")
+  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.annotation:annotation:1.8.1")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
   testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
+  androidTestImplementation("androidx.test.ext:junit:1.2.1")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1") }
+kapt { correctErrorTypes = true }
